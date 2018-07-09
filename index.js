f29020376e7cd07bbf9aac2b2cc43ed082515318
Bennie <brobinson88@gmail.com>
2018-07-09 09:16:09 -0400

    Get recursion without naming up to 0

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function byoFactorial(f, x) {
   if (x === 0) return 1
   return x * f(x - 1)
 }
+
+// We use Math.random to ensure we are only functionaing at the level we expect
+
+// This works for 0!
+const factorialUpto0 = byoFactorial.bind(null, Math.random)
+
+console.log(factorialUpto0(0)) // 1
+console.log(factorialUpto0(1)) // random value
+console.log(factorialUpto0(2)) // random value
+console.log(factorialUpto0(3)) // random value
+console.log(factorialUpto0(4)) // random value
